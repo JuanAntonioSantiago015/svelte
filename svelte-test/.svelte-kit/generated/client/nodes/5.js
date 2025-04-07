6cf270bb5d6bd1302a58dc82880a61db3ae00a30
JuanAntonioSantiago015 <juanantoniosantiago92@gmail.com>
2025-04-07 09:58:06 -0600

    first commit

--- svelte-test/.svelte-kit/generated/client/nodes/5.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/crud/update/[muniid]/+page.svelte";
--- svelte-test/.svelte-kit/generated/client/nodes/7.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/mail/loadData/+page.svelte";
